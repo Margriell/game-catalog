17d3f26f9459909a0da9022ef4b3fac8a53dfde0
Margriell <s99673@pollub.edu.pl>
2025-12-02 18:25:21 +0100

    Dodanie brakującej adnotacji dla ReviewRepository

--- src/main/java/com/gamecatalog/repository/ReviewRepository.java
+++ src/main/java/com/gamecatalog/repository/ReviewRepository.java
@@ -2,9 +2,11 @@ package com.gamecatalog.repository;
 
 import com.gamecatalog.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findAllByGameId(Long gameId);
 
